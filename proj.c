b221cdbd773b1b54c1ecc72cf1b72605eb4f02a1
vinay <136108213+VinayDesai-I@users.noreply.github.com>
2024-12-13 07:29:18 +0530

    proj.c

---
+++ proj.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<string.h>
+
+struct stu_info
+{
+    long int rollno;
+    char name[16];
+    char branch[10];
+    int sub_mks[100];
+    int t_mks;
+};
+
+int main(){
+
+    int n, i, j = 0, k, l, x, y, temp;
+    long int temp1;
+    char tn[16], tn1[10];
+    
+    printf("Enter The Number of Students to Add:");
+    scanf("%d", &n);
+
+    struct stu_info s[n];
+
+    for (i = 0; i < n; i++)
+    {
+        // full name
+        printf("\nEnter Full Name of Student:");
+        getchar();
+        fgets(s[i].name, sizeof(s[i].name), stdin);
+        s[i].name[strcspn(s[i].name, "\n")] = '\0';
+
+        // roll number
+        printf("Enter Student Roll Number:");
+        scanf("%d", &s[i].rollno);
+
+        // branch
+        printf("Enter Student Branch:");
+        scanf("%s", &s[i].branch);
+
+        // marks
+        s[i].t_mks = 0;
+        for (k = 0; k < 5; k++)
+        {
+            printf("Enter Marks of %d Subject:", k+1);
+            scanf("%d", &s[i].sub_mks[k]);
+            s[i].t_mks += s[i].sub_mks[k];
+        }
+
+    }
+
+    printf("Roll No. \tFull Name \tBranch \t\tTotal Marks\n");
+
+    for (l = 0; l < n; l++)
+    {
+        printf("\n%ld \t%s \t%s \t\t%d\n", s[l].rollno, s[l].name, s[l].branch, s[l].t_mks);
+    }
+
+
+    for (x = 1; x < n; x++)
+    {
+        for (y = 0; y < n - x; y++)
+        {
+            if (s[y].t_mks > s[y + 1].t_mks)
+            {
+                temp = s[y].t_mks;
+                strcpy(tn, s[y].name);
+                strcpy(tn1, s[y].branch);
+                temp1 = s[y].rollno;
+
+                s[y].t_mks = s[y +1].t_mks;
+                strcpy(s[y].name, s[y + 1].name);
+                strcpy(s[y].branch, s[y + 1].branch);
+                s[y].rollno = s[y + 1].rollno;
+
+                s[y + 1].t_mks = temp;
+                strcpy(s[y + 1].name, tn);
+                strcpy(s[y + 1].branch, tn1);
+                s[y + 1].rollno = temp1;
+            }
+        }
+    }
+
+    printf("\nRoll No \tName \t\tBranch \tTotal Marks");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("\n%ld \t%s \t\t%s \t%d", s[i].rollno, s[i].name, s[i].branch, s[i].t_mks);
+    }
+
+    return 0;
+}
